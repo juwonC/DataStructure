02240d8462a372af6977a6ee631a8273bc6dc147
JuwonCha <juwon1512@gmail.com>
2022-08-24 23:44:20 +0900

    Implement Queue using Array

--- Queue/main.cpp
+++ Queue/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 const int QUEUE_SIZE = 100;
 
@@ -10,7 +11,12 @@ struct Queue
 	int pTail{ -1 };
 };
 
-void UserInput(std::string input);
+void UserInput(Queue& q, std::string input);
+void Enqueue(Queue& q, int value);
+void Dequeue(Queue& q);
+void GetSize(Queue& q);
+void CheckEmpty(Queue& q);
+void Print(Queue& q);
 
 int main()
 {
@@ -26,7 +32,7 @@ int main()
 
 		std::cin >> input;
 
-		UserInput(input);
+		UserInput(myQueue, input);
 
 		if (input == "quit")
 		{
@@ -35,7 +41,89 @@ int main()
 	}
 }
 
-void UserInput(std::string input)
+void UserInput(Queue& q, std::string input)
 {
+	if (input == "enqueue")
+	{
+		int value;
 
+		std::cin >> value;
+		
+		Enqueue(q, value);
+	}
+	else if (input == "dequeue")
+	{
+		Dequeue(q);
+	}
+	else if (input == "size")
+	{
+		GetSize(q);
+	}
+	else if (input == "empty")
+	{
+		CheckEmpty(q);
+	}
+	else if (input == "print")
+	{
+		Print(q);
+	}
+	else
+	{
+		return;
+	}
+}
+
+void Enqueue(Queue& q, int value)
+{
+	if (q.pTail == QUEUE_SIZE - 1)
+	{
+		std::cout << "Queue is full" << '\n';
+		return;
+	}
+
+	q.container[++q.pTail] = value;
+}
+
+void Dequeue(Queue& q)
+{
+	if (q.pHead == q.pTail)
+	{
+		std::cout << "Queue is empty" << '\n';
+		return;
+	}
+
+	std::cout << q.container[++q.pHead] << '\n';
+}
+
+void GetSize(Queue& q)
+{
+	std::cout << q.pTail - q.pHead << '\n';
+}
+
+void CheckEmpty(Queue& q)
+{
+	if (q.pHead == q.pTail)
+	{
+		std::cout << 1 << '\n';
+	}
+	else
+	{
+		std::cout << 0 << '\n';
+	}
+}
+
+void Print(Queue& q)
+{
+	for (int i = 0; i < QUEUE_SIZE - 1; i++)
+	{
+		if (i <= q.pHead || i > q.pTail)
+		{
+			std::cout << " ";
+		}
+		else
+		{
+			std::cout << q.container[i] << " ";
+		}
+	}
+	std::cout << '\n';
 }
