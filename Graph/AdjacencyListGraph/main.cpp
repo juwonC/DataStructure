42ddb612b0d45686ca9776beac9ff0cbd43c9de7
JuwonCha <juwon1512@gmail.com>
2022-10-27 00:14:25 +0900

    Add Vertex, Edge, Graph struct

--- Graph/AdjacencyListGraph/main.cpp
+++ Graph/AdjacencyListGraph/main.cpp
@@ -1,6 +1,61 @@
 #include <iostream>
 
+struct Vertex
+{
+	int value;
+
+	Vertex* pNext;
+};
+
+struct Edge
+{
+	Edge* pNext;
+
+	Vertex* pSource;
+	Vertex* pDestination;
+};
+
+struct AdjacecnyListGraph
+{
+	int size;
+
+	Vertex* vertices;
+};
+
+Vertex* CreateVertex(int vertex)
+{
+	Vertex* node = new Vertex();
+
+	node->value = vertex;
+	node->pNext = nullptr;
+
+	return node;
+}
+
+Edge* CreateEdge(Vertex* u, Vertex* v)
+{
+	Edge* edge = new Edge();
+
+	edge->pNext = nullptr;
+	edge->pSource = u;
+	edge->pDestination = v;
+
+	return edge;
+}
+
+AdjacecnyListGraph* CreateAdjacecnyListGraph()
+{
+	AdjacecnyListGraph* graph = new AdjacecnyListGraph();
+
+	graph->size = 0;
+	graph->vertices = nullptr;
+
+	return graph;
+}
+
 int main()
 {
 	std::cout << "AdjacencyListGraph" << std::endl;
+
+
 }
